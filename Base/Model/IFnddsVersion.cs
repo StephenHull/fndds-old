04c9ca6b46c7f8657e0e4be769c4bc44273394b8
stephenhull <shull@stephenghull.com>
2017-07-01 14:04:14 -0400

    Created a base project to share common code.

---
+++ Base/Model/IFnddsVersion.cs
@@ -0,0 +1,19 @@
+﻿namespace Base.Model
+{
+    using System;
+
+    public interface IFnddsVersion
+    {
+        int Id { get; set; }
+
+        int BeginYear { get; set; }
+
+        int EndYear { get; set; }
+
+        int? Major { get; set; }
+
+        int? Minor { get; set; }
+
+        DateTime Created { get; set; }
+    }
+}
